44872852e825f0e131ffab597e638824016496f6
Justyn Hunter <cjhunter@me.com>
2018-12-17 17:48:37 -0600

    add newplayerform

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  state = {};
+  // state
+  state = {
+    players: [],
+    dateStarted: {}
+  };
 
   // events
 
---
+++ src/Components/Player/NewPlayerForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class NewPlayerForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+      name: '',
+      phone: '',
+    }
+
+    this.state = this.initialState;
+  }
+
+  handleChange = event => {
+    const {name, phone} = event.target;
+
+    this.setState({
+      [name] : phone
+    });
+  }
+
+  submitForm = () => {
+    this.props.handleSubmit(this.state);
+    this.setState(this.initialState);
+  }
+
+  render() {
+    const {name, phone} = this.state;
+
+    return(
+      <form>
+        <label>Name</label>
+        <input type="text" name="name" value={name} onChange={this.handleChange} />
+        <label>Phone</label>
+        <input type="text" name="phone" value={phone} onChange={this.handleChange} />
+
+        <input type="submit" value="Add Player" onClick={this.submitForm} />
+      </form>
+    )
+  }
+}
